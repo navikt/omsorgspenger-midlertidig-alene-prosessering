a9290c783b7c79831206702650a42e7aba0d78bb
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-10-15 13:58:00 +0200

    Oppdaterer dusseldorfKtorVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "1.3.2.4e29fb7"
+val dusseldorfKtorVersion = "1.4.1.4754df6"
 val k9FormatVersion = "3.0.0.f5ec313"
 val ktorVersion = ext.get("ktorVersion").toString()
 val slf4jVersion = ext.get("slf4jVersion").toString()
