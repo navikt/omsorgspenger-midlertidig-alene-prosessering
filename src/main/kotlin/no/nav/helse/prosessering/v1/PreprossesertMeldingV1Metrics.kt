fec6ddedba85fbef2c9ba87ad864b34a4229ce95
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-11-30 12:47:57 +0100

    Legger på metrikk som måler hvor mange som sender inn hvor de oppgir periode som er under 6 mnd

--- src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
@@ -18,7 +18,15 @@ private val generelCounter = Counter.build()
 internal fun PreprossesertMeldingV1.reportMetrics(){
     annenForelderSituasjonCounter.labels(annenForelder.situasjon.name).inc()
 
+    //Sjekker hvor mange som sender inn hvor de sier "Nei" til at perioden er over 6 mnd
     if(annenForelder.periodeOver6Måneder != null && !annenForelder.periodeOver6Måneder){
         generelCounter.labels("erPeriodenOver6MndSpørsmål", "Nei")
+    }
+
+    //Sjekker om hvor mange som sender inn perioder som er under 6 mnd
+    if(annenForelder.periodeFraOgMed != null && annenForelder.periodeTilOgMed != null){
+        if(!annenForelder.periodeFraOgMed.plusMonths(6).isBefore(annenForelder.periodeTilOgMed)){
+            generelCounter.labels("erPeriodenOver6Mnd", "Nei")
+        }
     }
 }
