c4c8dbe7fdf7a478fc226cfc004eedc8c3ad2895
Sander Opperud Odden <sander.opperud.odden@nav.no>
2020-12-01 11:17:50 +0100

    Legger på inc() på metrikker

--- src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertMeldingV1Metrics.kt
@@ -23,14 +23,14 @@ internal fun PreprossesertMeldingV1.reportMetrics(){
 
     //Sjekker hvor mange som sender inn hvor de sier "Nei" til at perioden er over 6 mnd
     if(annenForelder.periodeOver6Måneder != null && !annenForelder.periodeOver6Måneder){
-        generelCounter.labels("erPeriodenOver6MndSpørsmål", "Nei")
+        generelCounter.labels("erPeriodenOver6MndSpørsmål", "Nei").inc()
         logger.info("Metrikk: `erPeriodenOver6MndSpørsmål`, `Nei` økes med en")
     }
 
     //Sjekker om hvor mange som sender inn perioder som er under 6 mnd
     if(annenForelder.periodeFraOgMed != null && annenForelder.periodeTilOgMed != null){
         if(!annenForelder.periodeFraOgMed.plusMonths(6).isBefore(annenForelder.periodeTilOgMed)){
-            generelCounter.labels("erPeriodenOver6Mnd", "Nei")
+            generelCounter.labels("erPeriodenOver6Mnd", "Nei").inc()
             logger.info("Metrikk: `erPeriodenOver6Mnd`, `Nei` økes med en")
         }
     }
