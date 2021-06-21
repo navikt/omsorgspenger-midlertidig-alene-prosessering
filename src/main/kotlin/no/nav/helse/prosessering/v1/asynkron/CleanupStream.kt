17bfacc8fb8e32ac23eb7c166f06e2e85f01d831
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-21 12:10:38 +0200

    Fjerner filter

--- src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/CleanupStream.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/CleanupStream.kt
@@ -39,7 +39,6 @@ internal class CleanupStream(
             builder
                 .stream(fraCleanup.name, fraCleanup.consumed)
                 .filter { _, entry -> 1 == entry.metadata.version }
-                .filter { _, entry -> entry.metadata.correlationId != "generated-a6b9504a-b45d-4378-9450-b0dc93a71796" }
                 .mapValues { soknadId, entry ->
                     process(NAME, soknadId, entry) {
                         val cleanupMelding = entry.deserialiserTilCleanup()
