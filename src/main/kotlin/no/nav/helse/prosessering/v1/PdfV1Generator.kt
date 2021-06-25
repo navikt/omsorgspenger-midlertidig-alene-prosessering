c29a020a91052743ee5faff18f90b7b53f980f5b
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-06-25 12:44:53 +0200

    Gyldig PDF/A
    * Generere 1b pdf/a
    * Bruker useColorProfile og legger til sRGB.icc som fargeprofil
    
    * Fjerner ubrukt metoder
    
    * Bruker base64 encoda bilder i riktig format. Jpeg
    
    * Legger til riktig filer

--- src/main/kotlin/no/nav/helse/prosessering/v1/PdfV1Generator.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PdfV1Generator.kt
@@ -7,6 +7,7 @@ import com.github.jknack.handlebars.context.MapValueResolver
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader
 import com.openhtmltopdf.outputdevice.helper.BaseRendererBuilder
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
+import com.openhtmltopdf.util.XRLog
 import no.nav.helse.dusseldorf.ktor.core.fromResources
 import no.nav.helse.prosessering.v1.søknad.MeldingV1
 import no.nav.helse.prosessering.v1.søknad.Søker
@@ -19,7 +20,7 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
-import java.util.*
+import java.util.logging.Level
 
 internal class PdfV1Generator {
     private companion object {
@@ -30,11 +31,9 @@ internal class PdfV1Generator {
         private val BOLD_FONT = "$ROOT/fonts/SourceSansPro-Bold.ttf".fromResources().readBytes()
         private val ITALIC_FONT = "$ROOT/fonts/SourceSansPro-Italic.ttf".fromResources().readBytes()
 
-        private val images = loadImages()
+        private val sRGBColorSpace = "$ROOT/sRGB.icc".fromResources().readBytes()
+
         private val handlebars = Handlebars(ClassPathTemplateLoader("/$ROOT")).apply {
-            registerHelper("image", Helper<String> { context, _ ->
-                if (context == null) "" else images[context]
-            })
             registerHelper("eq", Helper<String> { context, options ->
                 if (context == options.param(0)) options.fn() else options.inverse()
             })
@@ -60,23 +59,10 @@ internal class PdfV1Generator {
 
         private val ZONE_ID = ZoneId.of("Europe/Oslo")
         private val DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm").withZone(ZONE_ID)
-
-        private fun loadPng(name: String): String {
-            val bytes = "$ROOT/images/$name.png".fromResources().readBytes()
-            val base64string = Base64.getEncoder().encodeToString(bytes)
-            return "data:image/png;base64,$base64string"
-        }
-
-        private fun loadImages() = mapOf(
-            "Hjelp.png" to loadPng("Hjelp"),
-            "Navlogo.png" to loadPng("Navlogo"),
-            "Personikon.png" to loadPng("Personikon"),
-            "Fritekst.png" to loadPng("Fritekst")
-        )
     }
 
     internal fun generateSoknadOppsummeringPdf(melding: MeldingV1): ByteArray {
-        //XRLog.setLoggingEnabled(false) //TODO Finnes det en måte å kun justere logg level, ikke skru den helt av?
+        XRLog.listRegisteredLoggers().forEach { logger -> XRLog.setLevel(logger, Level.WARNING) }
         soknadTemplate.apply(
             Context
                 .newBuilder(
@@ -109,6 +95,8 @@ internal class PdfV1Generator {
             PdfRendererBuilder()
                 .useFastMode()
                 .usePdfUaAccessbility(true)
+                .usePdfAConformance(PdfRendererBuilder.PdfAConformance.PDFA_1_B)
+                .useColorProfile(sRGBColorSpace)
                 .withHtmlContent(html, "")
                 .medFonter()
                 .toStream(outputStream)
